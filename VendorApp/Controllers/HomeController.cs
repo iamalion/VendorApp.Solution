648f6231828c2e3f94e1d2609570a184c8bb6950
Lindsay Warr <iamalion@gmail.com>
2023-07-21 10:47:47 -0700

    Add all base files

---
+++ VendorApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using MusicLibrary.Models;
+
+namespace MusicLibrary.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
