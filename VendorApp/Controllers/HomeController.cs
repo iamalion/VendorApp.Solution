58eb9a2a80495fcb9c07f9bf1bb56f64cd3c7ebe
Lindsay Warr <iamalion@gmail.com>
2023-07-21 11:01:13 -0700

    Add Vendor model and test

--- VendorApp/Controllers/HomeController.cs
+++ VendorApp/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using MusicLibrary.Models;
+using VendorApp.Models;
 
-namespace MusicLibrary.Controllers
+namespace VendorApp.Controllers
 {
     public class HomeController : Controller
     {
